387b35c619c73bb5c6d8e6069a041c394daffff9
Jonathan Gluck <jonathangluck08854@gmail.com>
2015-04-12 08:25:24 -0400

    pushing fix for weird padding issue

--- js/assem.js
+++ js/assem.js
@@ -1,68 +1,69 @@
 var ASSEM = [];
 
 (function() {
-  function padq(n) {
-    if (n.length === 8) return n;
-    else return toBigEndian(padHex(n, 8));
-  }
+	function padq(n) {
+		if (n.length === 8) return n;
+		// else return toBigEndian(padHex(n, 8));
+		else return padLEHex(n, 8);
+	}
 
-  ASSEM[0] = function() {
-    return '00';
-  };
+	ASSEM[0] = function() {
+		return '00';
+	};
 
-  ASSEM[1] = function() {
-    return '10';
-  };
+	ASSEM[1] = function() {
+		return '10';
+	};
 
-  ASSEM[2] = function() {
-    return '2' + this.fn + this.rA + this.rB;
-  };
+	ASSEM[2] = function() {
+		return '2' + this.fn + this.rA + this.rB;
+	};
 
-  ASSEM[3] = function() {
-    return '30f' + this.rB + padq(this.V);
-  };
+	ASSEM[3] = function() {
+		return '308' + this.rB + padq(this.V);
+	};
 
-  ASSEM[4] = function() {
-    return '40' + this.rA + this.rB + padq(this.D);
-  };
+	ASSEM[4] = function() {
+		return '40' + this.rA + this.rB + padq(this.D);
+	};
 
-  ASSEM[5] = function() {
-    if(this.D == -1){
-      return '50' + this.rA + this.rB
-    }else{
-      return '50' + this.rA + this.rB + padq(this.D);
-    }
-  };
+	ASSEM[5] = function() {
+		if (this.D == -1) {
+			return '50' + this.rA + this.rB
+		} else {
+			return '50' + this.rA + this.rB + padq(this.D);
+		}
+	};
 
-  ASSEM[6] = function() {
-    return '6' + this.fn + this.rA + this.rB;
-  };
+	ASSEM[6] = function() {
+		return '6' + this.fn + this.rA + this.rB;
+	};
 
-  ASSEM[7] = function() {
-    return '7' + this.fn + padq(this.Dest);
-  };
+	ASSEM[7] = function() {
+		return '7' + this.fn + padq(this.Dest);
+	};
 
-  ASSEM[8] = function() {
-    return '80' + padq(this.Dest);
-  };
+	ASSEM[8] = function() {
+		return '80' + padq(this.Dest);
+	};
 
-  ASSEM[9] = function() {
-    return '90';
-  };
+	ASSEM[9] = function() {
+		return '90';
+	};
 
-  ASSEM[10] = function() {
-    return 'a0' + this.rA + 'f';
-  };
+	ASSEM[10] = function() {
+		return 'a0' + this.rA + 'f';
+	};
 
-  ASSEM[11] = function() {
-    return 'b0' + this.rA + 'f';
-  };
+	ASSEM[11] = function() {
+		return 'b0' + this.rA + 'f';
+	};
 
-  ASSEM[14] = function() {
-    return 'e' + this.fn;
-  }
+	ASSEM[14] = function() {
+		return 'e' + this.fn;
+	}
 
-  ASSEM[15] = function() {
-    return 'f' + this.fn + this.rA + 'f';
-  }
+	ASSEM[15] = function() {
+		return 'f' + this.fn + this.rA + 'f';
+	}
 })();
--- js/instr.js
+++ js/instr.js
@@ -1,310 +1,310 @@
 var INSTR = {};
 
 INSTR[0] = function() {
-  STAT = 'HLT';
-  //print("Program halted");
+	STAT = 'HLT';
+	//print("Program halted");
 };
 INSTR[1] = function() {
-  //NOP
+	//NOP
 };
 INSTR[2] = function() {
-  switch (this.fn) {
-    case 0:
-      // RRMOVL
-      REG[this.rB] = getRegister(this.rA);
-      break;
-    case 1:
-      // CMOVLE
-      if (SF === 1 || ZF === 1) {
-        REG[this.rB] = getRegister(this.rA);
-      }
-      break;
-    case 2:
-      // CMOVL
-      if (SF === 1) {
-        REG[this.rB] = getRegister(this.rA);
-      }
-      break;
-    case 3:
-      // CMOVE
-      if (ZF === 1) {
-        REG[this.rB] = getRegister(this.rA);
-      }
-      break;
-    case 4:
-      // CMOVNE
-      if (ZF === 0) {
-        REG[this.rB] = getRegister(this.rA);
-      }
-      break;
-    case 5:
-      // CMOVGE
-      if (SF === 0 || ZF === 1) {
-        getRegister(this.rB) = getRegister(this.rA);
-      }
-      break;
-    case 6:
-      // CMOVG
-      if (SF === 0 && ZF === 0) {
-        REG[this.rB] = getRegister(this.rA);
-      }
-      break;
-  }
+	switch (this.fn) {
+		case 0:
+			// RRMOVL
+			REG[this.rB] = getRegister(this.rA);
+			break;
+		case 1:
+			// CMOVLE
+			if (SF === 1 || ZF === 1) {
+				REG[this.rB] = getRegister(this.rA);
+			}
+			break;
+		case 2:
+			// CMOVL
+			if (SF === 1) {
+				REG[this.rB] = getRegister(this.rA);
+			}
+			break;
+		case 3:
+			// CMOVE
+			if (ZF === 1) {
+				REG[this.rB] = getRegister(this.rA);
+			}
+			break;
+		case 4:
+			// CMOVNE
+			if (ZF === 0) {
+				REG[this.rB] = getRegister(this.rA);
+			}
+			break;
+		case 5:
+			// CMOVGE
+			if (SF === 0 || ZF === 1) {
+				getRegister(this.rB) = getRegister(this.rA);
+			}
+			break;
+		case 6:
+			// CMOVG
+			if (SF === 0 && ZF === 0) {
+				REG[this.rB] = getRegister(this.rA);
+			}
+			break;
+	}
 };
 INSTR[3] = function() {
-  REG[this.rB] = this.V;
+	REG[this.rB] = this.V;
 };
 INSTR[4] = function() {
-  var valA = getRegister(this.rA)
-  if (this.rB == 8) { //label move
-    ST(this.D, valA, 4);
-  } else {
-    var valB = getRegister(this.rB),
-      valE = valB + this.D;
-    ST(valE, valA, 4);
-  }
+	var valA = getRegister(this.rA)
+	if (this.rB == 8) { //label move
+		ST(this.D, valA, 4);
+	} else {
+		var valB = getRegister(this.rB),
+			valE = valB + this.D;
+		ST(valE, valA, 4);
+	}
 };
 INSTR[5] = function() {
-  if (this.rB == 8) { //label move
-    REG[this.rA] = LD(this.D);
-  } else {
-    var valB = getRegister(this.rB),
-      valE = valB + this.D;
-    REG[this.rA] = LD(valE);
-  }
+	if (this.rB == 8) { //label move
+		REG[this.rA] = LD(this.D);
+	} else {
+		var valB = getRegister(this.rB),
+			valE = valB + this.D;
+		REG[this.rA] = LD(valE);
+	}
 };
 
 INSTR[6] = function() {
-  var valA = getRegister(this.rA),
-    valB = getRegister(this.rB),
-    sgnA, sgnB, sgnR, signBit = 0x80000000;
-  switch (this.fn) {
-    case 0:
-      sgnA = !!(valA & signBit);
-      sgnB = !!(valB & signBit);
-      REG[this.rB] += getRegister(this.rA);
-      sgnR = !!(getRegister(this.rB) & signBit);
-      OF = +(sgnA && sgnB && !sgnR ||
-        !sgnA && !sgnB && sgnR)
-      break;
-    case 1:
-      sgnA = !!(valA & signBit);
-      sgnB = !!(valB & signBit);
-      REG[this.rB] -= getRegister(this.rA);
-      sgnR = !!(getRegister(this.rB) & signBit);
-      OF = +(sgnA && sgnB && !sgnR ||
-        !sgnA && !sgnB && sgnR)
-      break;
-    case 2:
-      REG[this.rB] = getRegister(this.rA) & getRegister(this.rB);
-      break;
-    case 3:
-      REG[this.rB] = getRegister(this.rA) ^ getRegister(this.rB);
-      break;
-    case 4:
-      REG[this.rB] = getRegister(this.rA) * getRegister(this.rB);
-      break;
-    case 5:
-      REG[this.rB] = getRegister(this.rB) / getRegister(this.rA);
-      break;
-    case 6:
-      REG[this.rB] = getRegister(this.rB) % getRegister(this.rA);
-      break;
-  }
-  SF = getRegister(this.rB) & 0x80000000 ? 1 : 0;
-  ZF = getRegister(this.rB) === 0 ? 1 : 0;
+	var valA = getRegister(this.rA),
+		valB = getRegister(this.rB),
+		sgnA, sgnB, sgnR, signBit = 0x80000000;
+	switch (this.fn) {
+		case 0:
+			sgnA = !!(valA & signBit);
+			sgnB = !!(valB & signBit);
+			REG[this.rB] += getRegister(this.rA);
+			sgnR = !!(getRegister(this.rB) & signBit);
+			OF = +(sgnA && sgnB && !sgnR ||
+				!sgnA && !sgnB && sgnR)
+			break;
+		case 1:
+			sgnA = !!(valA & signBit);
+			sgnB = !!(valB & signBit);
+			REG[this.rB] -= getRegister(this.rA);
+			sgnR = !!(getRegister(this.rB) & signBit);
+			OF = +(sgnA && sgnB && !sgnR ||
+				!sgnA && !sgnB && sgnR)
+			break;
+		case 2:
+			REG[this.rB] = getRegister(this.rA) & getRegister(this.rB);
+			break;
+		case 3:
+			REG[this.rB] = getRegister(this.rA) ^ getRegister(this.rB);
+			break;
+		case 4:
+			REG[this.rB] = getRegister(this.rA) * getRegister(this.rB);
+			break;
+		case 5:
+			REG[this.rB] = getRegister(this.rB) / getRegister(this.rA);
+			break;
+		case 6:
+			REG[this.rB] = getRegister(this.rB) % getRegister(this.rA);
+			break;
+	}
+	SF = getRegister(this.rB) & 0x80000000 ? 1 : 0;
+	ZF = getRegister(this.rB) === 0 ? 1 : 0;
 };
 INSTR[7] = function() {
-  switch (this.fn) {
-    case 0:
-      // JMP
-      PC = this.Dest;
-      break;
-    case 1:
-      // JLE
-      if (SF === 1 || ZF === 1) {
-        PC = this.Dest;
-      }
-      break;
-    case 2:
-      // JL
-      if (SF === 1) {
-        PC = this.Dest;
-      }
-      break;
-    case 3:
-      // JE
-      if (ZF === 1) {
-        PC = this.Dest;
-      }
-      break;
-    case 4:
-      // JNE
-      if (ZF === 0) {
-        PC = this.Dest;
-      }
-      break;
-    case 5:
-      // JGE
-      if (SF === 0 || ZF === 1) {
-        PC = this.Dest;
-      }
-      break;
-    case 6:
-      // JG
-      if (SF === 0 && ZF === 0) {
-        PC = this.Dest;
-      }
-      break;
-  }
+	switch (this.fn) {
+		case 0:
+			// JMP
+			PC = this.Dest;
+			break;
+		case 1:
+			// JLE
+			if (SF === 1 || ZF === 1) {
+				PC = this.Dest;
+			}
+			break;
+		case 2:
+			// JL
+			if (SF === 1) {
+				PC = this.Dest;
+			}
+			break;
+		case 3:
+			// JE
+			if (ZF === 1) {
+				PC = this.Dest;
+			}
+			break;
+		case 4:
+			// JNE
+			if (ZF === 0) {
+				PC = this.Dest;
+			}
+			break;
+		case 5:
+			// JGE
+			if (SF === 0 || ZF === 1) {
+				PC = this.Dest;
+			}
+			break;
+		case 6:
+			// JG
+			if (SF === 0 && ZF === 0) {
+				PC = this.Dest;
+			}
+			break;
+	}
 };
 INSTR[8] = function() {
-  var valB = getRegister(4),
-    valE = valB - 4;
-  ST(valE, PC, 4);
-  REG[4] = valE;
-  PC = this.Dest;
+	var valB = getRegister(4),
+		valE = valB - 4;
+	ST(valE, PC, 4);
+	REG[4] = valE;
+	PC = this.Dest;
 };
 INSTR[9] = function() {
-  var valA = getRegister(4),
-    valB = getRegister(4),
-    valE = valB + 4,
-    valM = LD(valA);
-  REG[4] = valE;
-  PC = valM;
+	var valA = getRegister(4),
+		valB = getRegister(4),
+		valE = valB + 4,
+		valM = LD(valA);
+	REG[4] = valE;
+	PC = valM;
 };
 INSTR[10] = function() {
-  var valA = getRegister(this.rA),
-    valB = getRegister(4),
-    valE = valB - 4;
-  ST(valE, valA, 4);
-  REG[4] = valE;
+	var valA = getRegister(this.rA),
+		valB = getRegister(4),
+		valE = valB - 4;
+	ST(valE, valA, 4);
+	REG[4] = valE;
 };
 INSTR[11] = function() {
-  var valA = getRegister(4),
-    valB = getRegister(4),
-    valE = valB + 4,
-    valM = LD(valA);
-  REG[4] = valE;
-  REG[this.rA] = valM;
+	var valA = getRegister(4),
+		valB = getRegister(4),
+		valE = valB + 4,
+		valM = LD(valA);
+	REG[4] = valE;
+	REG[this.rA] = valM;
 };
 
 INSTR[14] = function() {
-  switch (this.fn) {
-    case 0:
-      // BRK
-      STAT = 'DBG';
-      break;
-    case 1:
-      // BRKLE
-      if (SF === 1 || ZF === 1) {
-        STAT = 'DBG';
-      }
-      break;
-    case 2:
-      // BRKL
-      if (SF === 1) {
-        STAT = 'DBG';
-      }
-      break;
-    case 3:
-      // BRKE
-      if (ZF === 1) {
-        STAT = 'DBG';
-      }
-      break;
-    case 4:
-      // BRKNE
-      if (ZF === 0) {
-        STAT = 'DBG';
-      }
-      break;
-    case 5:
-      // BRKGE
-      if (SF === 0 || ZF === 1) {
-        STAT = 'DBG';
-      }
-      break;
-    case 6:
-      // BRKG
-      if (SF === 0 && ZF === 0) {
-        STAT = 'DBG';
-      }
-      break;
-  }
+	switch (this.fn) {
+		case 0:
+			// BRK
+			STAT = 'DBG';
+			break;
+		case 1:
+			// BRKLE
+			if (SF === 1 || ZF === 1) {
+				STAT = 'DBG';
+			}
+			break;
+		case 2:
+			// BRKL
+			if (SF === 1) {
+				STAT = 'DBG';
+			}
+			break;
+		case 3:
+			// BRKE
+			if (ZF === 1) {
+				STAT = 'DBG';
+			}
+			break;
+		case 4:
+			// BRKNE
+			if (ZF === 0) {
+				STAT = 'DBG';
+			}
+			break;
+		case 5:
+			// BRKGE
+			if (SF === 0 || ZF === 1) {
+				STAT = 'DBG';
+			}
+			break;
+		case 6:
+			// BRKG
+			if (SF === 0 && ZF === 0) {
+				STAT = 'DBG';
+			}
+			break;
+	}
 };
 
 INSTR[15] = function() {
-  switch (this.fn) {
-    case 0:
-      // rdch
-      window.input_flag = false
-      $("#input_box").prop("disabled", false);
-      $("#input_box").attr('maxlength', 1);
-      $("#input_box").focus()
-      STAT = 'I/O'
-      var timer = setInterval(function(rA) {
-        box = $("#input_box")
-        if (window.input_flag) {
-          if (box.val() == "") {
-            window.REG[rA] = 10;
-          } else {
-            window.REG[rA] = box.val().charCodeAt(0);
-          }
-          window.input_flag = false
-          box.val("")
-          box.prop("disabled", true)
-          window.STAT = 'AOK'
-          Backbone.Events.trigger('app:redraw');
-          clearInterval(timer);
-        }
-      }, 100, this.rA);
+	switch (this.fn) {
+		case 0:
+			// rdch
+			window.input_flag = false
+			$("#input_box").prop("disabled", false);
+			$("#input_box").attr('maxlength', 1);
+			$("#input_box").focus()
+			STAT = 'I/O'
+			var timer = setInterval(function(rA) {
+				box = $("#input_box")
+				if (window.input_flag) {
+					if (box.val() == "") {
+						window.REG[rA] = 10;
+					} else {
+						window.REG[rA] = box.val().charCodeAt(0);
+					}
+					window.input_flag = false
+					box.val("")
+					box.prop("disabled", true)
+					window.STAT = 'AOK'
+					Backbone.Events.trigger('app:redraw');
+					clearInterval(timer);
+				}
+			}, 100, this.rA);
 
-      break;
-    case 1:
-      // wrch
-      out_s = String.fromCharCode(getRegister(this.rA))
-        // alert(out_s);
-      print(out_s);
-      window.OUTPUT += out_s
-      window.app.vent.trigger("output:updated")
-      break;
-    case 2:
-      // rdint
-      window.input_flag = false
-      $("#input_box").prop("disabled", false);
-      $("#input_box").attr('maxlength', 80);
-      $("#input_box").focus()
-      STAT = 'I/O'
-      var timer = setInterval(function(rA) {
-        box = $("#input_box")
-        if (window.input_flag) {
-          result = parseInt(box.val());
-          if (result == NaN) {
-            box.val("");
-          } else if (Number(result) === result && result % 1 === 0) { //checknumbernss
-            window.REG[rA] = result;
-            window.input_flag = false
-            box.val("")
-            box.prop("disabled", true)
-            window.STAT = 'AOK'
-            Backbone.Events.trigger('app:redraw');
-            clearInterval(timer);
-          } else {
-            box.val("")
-          }
-        }
-      }, 100, this.rA);
+			break;
+		case 1:
+			// wrch
+			out_s = String.fromCharCode(getRegister(this.rA))
+				// alert(out_s);
+			print(out_s);
+			window.OUTPUT += out_s
+			window.app.vent.trigger("output:updated")
+			break;
+		case 2:
+			// rdint
+			window.input_flag = false
+			$("#input_box").prop("disabled", false);
+			$("#input_box").attr('maxlength', 80);
+			$("#input_box").focus()
+			STAT = 'I/O'
+			var timer = setInterval(function(rA) {
+				box = $("#input_box")
+				if (window.input_flag) {
+					window.input_flag = false
+					result = parseInt(box.val());
+					if (result == NaN) {
+						box.val("");
+					} else if (Number(result) === result && result % 1 === 0) { //checknumbernss
+						window.REG[rA] = result;
+						box.val("")
+						box.prop("disabled", true)
+						window.STAT = 'AOK'
+						Backbone.Events.trigger('app:redraw');
+						clearInterval(timer);
+					} else {
+						box.val("")
+					}
+				}
+			}, 100, this.rA);
 
 
-      break;
-    case 3:
-      //wrint
-      out_s = getRegister(this.rA).toString()
-        // alert(out_s);
-      print(out_s);
-      window.OUTPUT += out_s
-      window.app.vent.trigger("output:updated")
-      break;
-  }
+			break;
+		case 3:
+			//wrint
+			out_s = getRegister(this.rA).toString()
+				// alert(out_s);
+			print(out_s);
+			window.OUTPUT += out_s
+			window.app.vent.trigger("output:updated")
+			break;
+	}
 };
