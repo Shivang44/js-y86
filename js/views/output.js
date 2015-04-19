fd15f1a679d67d91cc482c1f5005877b6a86fe34
Jonathan Gluck <jonathangluck08854@gmail.com>
2015-04-18 23:09:05 -0400

    pushing a fix for newlines

--- js/views/output.js
+++ js/views/output.js
@@ -19,29 +19,29 @@
 // SOFTWARE.
 
 var OutputView = Backbone.View.extend({
-	className: 'output',
+  className: 'output',
 
-	initialize: function() {
-		this.template = _.template($('#tmpl_output').html());
-		this.output = "hi";
-		this.render(this);
+  initialize: function() {
+    this.template = _.template($('#tmpl_output').html());
+    this.output = "hi";
+    this.render(this);
 
-		window.app.vent.on("output:updated", this.render, this)
-			// this.$el.html( this.template );
-	},
+    window.app.vent.on("output:updated", this.render, this)
+      // this.$el.html( this.template );
+  },
 
-	render: function(context) {
+  render: function(context) {
 
-		if (!this.rendered) {
-			// this.$el.empty().append(this.template());
-			this.rendered = true;
-			this.$lineContainer = this.$('.lines');
-		}
-		this.output = {
-			output: window.OUTPUT.replace("\n", "<br>")
-		};
-		this.$el.empty().append(this.template(this.output))
-	},
+    if (!this.rendered) {
+      // this.$el.empty().append(this.template());
+      this.rendered = true;
+      this.$lineContainer = this.$('.lines');
+    }
+    this.output = {
+      output: window.OUTPUT.replace(/\n/g, "<br>")
+    };
+    this.$el.empty().append(this.template(this.output))
+  },
 
 
 
